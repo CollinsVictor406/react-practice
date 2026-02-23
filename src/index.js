7ab19530f08607bf85ed64c5827324b66ab9fec1
collins <collinsv406@gmail.com>
2026-02-24 00:18:35 +0100

    implemented the better props logic using spread operator

--- src/index.js
+++ src/index.js
@@ -2,23 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-    const title = 'The Story of My Life';
-    const author = 'Collins Victor';
-    const image = 'https://m.media-amazon.com/images/I/81niTZGXxXL._AC_UL480_FMwebp_QL65_.jpg';
+
+const books = [ 
+    {
+    title: 'The Story of My Life',
+    author: 'Collins Victor',
+    image: 'https://m.media-amazon.com/images/I/81niTZGXxXL._AC_UL480_FMwebp_QL65_.jpg',
+    id:1
+},
+ {
+    title: 'Lost',
+    author: 'Ehis Victor',
+    image: 'https://m.media-amazon.com/images/I/81AnqpwwO1L._AC_UL480_FMwebp_QL65_.jpg',
+    id:2,
+},    {
+    title: 'The Girl Behind the Gates: The gripping, heartbreaking historical bestseller based on a true',
+    author: 'Daniel Victor',
+    image: 'https://m.media-amazon.com/images/I/91jNTrkt6cL._AC_UL480_FMwebp_QL65_.jpg',
+    id:3
+}
+
+]
+
 
 const BookList = () =>{
   return <section className='booklist'>
-          <Book title={title} author={author} image={image}/>
-          <Book title={title} author={author} image={image}/>
+       <EventExample />
+       {books.map((book)=>{
+        // const { title, author, image, id} = book
+        return <Book {...book}  key={book.id}/>
+       })}
   </section>
 }
- 
-const Book = (props) =>{ 
-   console.log(props);
+const EventExample = () =>{
+    return <section>
+       <form>
+        <h2>Typical Form</h2>
+        <input type='text' name='example' style={{margin:'1rem'}}></input>
+       </form>
+       <button>Click me</button>
+    </section>
+}
+const Book = ({image, title, author,children}) =>{ 
+//    const {image, title, author} = props
     return <article className='book'>
-     <img src={props.image} alt='story'></img>
-     <h2>{props.title}</h2>
-        <h4>{props.author}</h4>
+       <img src={image} alt='story'></img>
+       <h2>{title}</h2>
+       <h4>{author}</h4>
+           {children}
     </article>}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

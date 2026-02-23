966f303ff5aee4cebb0f46e4c87dac60ed4fa8cb
collins <collinsv406@gmail.com>
2026-02-23 13:49:40 +0100

    added css to my app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const BookList = () =>{
-  return <section>
+  return <section className='booklist'>
        <Book></Book>
        <Book></Book>
        <Book></Book>
        <Book></Book>
   </section>
 }
-
+ 
 const Book = () =>{ 
-    return <article>
+    return <article className='book'>
      <Image></Image>
      <Title></Title>
      <Author></Author>

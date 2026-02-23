e3a4f8b87b601b7ce79a2063b0f6c02a604ba010
collins <collinsv406@gmail.com>
2026-02-23 12:37:42 +0100

    first rendering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
-  return <div>
-         <Person></Person>
-          <Message></Message>
-          </div>
+const BookList = () =>{
+  return <section>
+       <Book></Book>
+       <Book></Book>
+       <Book></Book>
+       <Book></Book>
+  </section>
 }
 
-const Person = () => <h1>My name is Collins Victor</h1>
-const Message = () =>  <p>i come from edo state</p>
+const Book = () =>{ 
+    return <article>
+     <Image></Image>
+     <Title></Title>
+     <Author></Author>
+    </article>}
+const Image = () =>{
+    return <img src='https://m.media-amazon.com/images/I/81niTZGXxXL._AC_UL480_FMwebp_QL65_.jpg' alt='story'></img>
+}
+const Title = () =>{
+    return <h2>Failure is Part of the Journey</h2>
+}
+const Author = () =>{
+    return <h2>Collins Victor</h2>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);

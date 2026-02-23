12e421d1f17e53bc64fa6cde28136eb37250ad5a
collins <collinsv406@gmail.com>
2026-02-23 12:07:22 +0100

    learning react basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function Greeting() {
+  return <div>
+         <Person></Person>
+          <Message></Message>
+          </div>
+}
+
+const Person = () => <h1>My name is Collins Victor</h1>
+const Message = () =>  <p>i come from edo state</p>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting />);
